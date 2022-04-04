f5e1aef180df2f4207cf382fb37cb99ec7ee8d23
Dominik Slehofer <slehoferdominik@gmail.com>
2022-04-04 13:42:36 +0200

    luxus

--- kalkulacka.py
+++ kalkulacka.py
@@ -4,89 +4,114 @@
 # Autor:   Marek Nožka, nozka <@t> spseol <d.t> cz
 ############################################################################
 import math
-
-zasobnik = []
-
-
-def operace(token):
-    if token.upper() == "Q":
-        exit()
-    if token.upper() == "PI":
-        zasobnik.append(math.pi)
-    if token == "+":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(a + b)
-    if token == "-":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(a - b)
-    if token == "*":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(a * b)
-    if token == "**":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(a ** b)
-    if token == "sin":
-        a = zasobnik.pop()
-        zasobnik.append(math.sin(a))
-    if token == "cos":
-        a = zasobnik.pop()
-        zasobnik.append(math.cos(a))
+from importlib.metadata import PathDistribution
+from os.path import basename, splitext
+import tkinter as tk
+from tkinter import messagebox, IntVar
+from tkinter.messagebox import 
 
 
-dva_operandy = {}
-dva_operandy["+"] = lambda a, b: a + b
-dva_operandy["-"] = lambda a, b: a - b
-dva_operandy["*"] = lambda a, b: a * b
-dva_operandy["/"] = lambda a, b: a / b
-dva_operandy["//"] = lambda a, b: a // b
-dva_operandy["**"] = lambda a, b: a ** b
-
-jeden_operand = {}
-jeden_operand["sin"] = math.sin
-jeden_operand["cos"] = math.cos
-jeden_operand["tg"] = math.tan
-jeden_operand["tan"] = math.tan
+class Application(tk.Tk):
+    name = basename(splitext(basename(__file__.capitalize()))[0])
+    name = "grafy"
+    
+    
+    
 
 
-def operace(token):
-    if token.upper() == "Q":
-        exit()
-    if token.upper() == "PI":
-        zasobnik.append(math.pi)
-    if token.upper() == "SW":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(b)
-        zasobnik.append(a)
-    if token in dva_operandy.keys():
-        if len(zasobnik) >= 2:
-            b = zasobnik.pop()
-            a = zasobnik.pop()
-            zasobnik.append(dva_operandy[token](a, b))
-        else:
-            print("Nemám dost operandů!!!")
-    if token in jeden_operand.keys():
-        if len(zasobnik) >= 1:
-            a = zasobnik.pop()
-            zasobnik.append(jeden_operand[token](a))
-        else:
-            print("Nemám dost operandů!!!")
+    def __init__(self):
+        super().__init__(className=self.name)
+        self.title(self.name)
+        self.bind("<Escape>", self.quit)
+        self.geometry("400x400")
+
+        self.dva_operandy = {}
+        self.dva_operandy["+"] = lambda a, b: a + b
+        self.dva_operandy["-"] = lambda a, b: a - b
+        self.dva_operandy["*"] = lambda a, b: a * b
+        self.dva_operandy["/"] = lambda a, b: a / b
+        self.dva_operandy["//"] = lambda a, b: a // b
+        self.dva_operandy["**"] = lambda a, b: a ** b
+
+        self.jeden_operand = {}
+        self.jeden_operand["sin"] = math.sin
+        self.jeden_operand["cos"] = math.cos
+        self.jeden_operand["tg"] = math.tan
+        self.jeden_operand["tan"] = math.tan
+
+        self.zasobnik= []
+
+    
 
 
-def zpracuj(radek):
-    tokeny = radek.split()
-    for token in tokeny:
-        try:
-            zasobnik.append(float(token))
-        except ValueError:
-            operace(token)
+    def operace(self,token):
+        if token.upper() == "Q":
+            exit()
+        if token.upper() == "PI":
+            self.zasobnik.append(math.pi)
+        if token == "+":
+            b = self.zasobnik.pop()
+            a = self.zasobnik.pop()
+            self.zasobnik.append(a + b)
+        if token == "-":
+            b = self.zasobnik.pop()
+            a = self.zasobnik.pop()
+            self.zasobnik.append(a - b)
+        if token == "*":
+            b = self.zasobnik.pop()
+            a = self.zasobnik.pop()
+            self.zasobnik.append(a * b)
+        if token == "**":
+            b = self.zasobnik.pop()
+            a = self.zasobnik.pop()
+            self.zasobnik.append(a ** b)
+        if token == "sin":
+            a = self.zasobnik.pop()
+            self.zasobnik.append(math.sin(a))
+        if token == "cos":
+            a = self.zasobnik.pop()
+            self.zasobnik.append(math.cos(a))
 
 
-# čtu vstup
-while True:
-    radek = input(zasobnik.__repr__() + ">>> ")
-    zpracuj(radek)
+
+
+
+    def operace(self, token):
+        if token.upper() == "Q":
+            exit()
+        if token.upper() == "PI":
+            self.zasobnik.append(math.pi)
+        if token.upper() == "SW":
+            b = self.zasobnik.pop()
+            a = self.zasobnik.pop()
+            self.zasobnik.append(b)
+            self.zasobnik.append(a)
+        if token in self.dva_operandy.keys():
+            if len(self.zasobnik) >= 2:
+                b = self.zasobnik.pop()
+                a = self.zasobnik.pop()
+                self.zasobnik.append(self.dva_operandy[token](a, b))
+            else:
+                print("Nemám dost operandů!!!")
+        if token in self.jeden_operand.keys():
+            if len(self.zasobnik) >= 1:
+                a = self.zasobnik.pop()
+                self.zasobnik.append(self.jeden_operand[token](a))
+            else:
+                print("Nemám dost operandů!!!")
+
+
+    def zpracuj(self,radek):
+        tokeny = radek.split()
+        for token in tokeny:
+            try:
+                self.zasobnik.append(float(token))
+            except ValueError:
+                self.operace(token)
+
+
+    # čtu vstup
+    while True:
+        pass
+        #radek = input(self.zasobnik.__repr__() + ">>> ")
+        #zpracuj(radek)
