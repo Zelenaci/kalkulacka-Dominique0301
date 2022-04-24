fe2b0242018d09cdd445b7fd98e700635b4d81da
Dominik Šlehofer <slehoferdominik@gmail.com>
2022-04-24 16:05:22 +0200

    doma

--- kalkulacka.py
+++ kalkulacka.py
@@ -2,9 +2,22 @@ import math
 from importlib.metadata import PathDistribution
 from os.path import basename, splitext
 import tkinter as tk
-from tkinter import messagebox, IntVar
+from tkinter import messagebox, IntVar, END
 from tkinter.messagebox import showerror
 
+dva_operandy = {}
+dva_operandy["+"] = lambda a, b: a + b
+dva_operandy["-"] = lambda a, b: a - b
+dva_operandy["*"] = lambda a, b: a * b
+dva_operandy["/"] = lambda a, b: a / b
+dva_operandy["//"] = lambda a, b: a // b
+dva_operandy["**"] = lambda a, b: a ** b
+
+jeden_operand = {}
+jeden_operand["sin"] = math.sin
+jeden_operand["cos"] = math.cos
+jeden_operand["tg"] = math.tan
+jeden_operand["tan"] = math.tan
 
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
@@ -20,8 +33,16 @@ class Application(tk.Tk):
         self.bind("<Escape>", self.quit)
         self.geometry("400x400")
 
-        self.vstup =tk.Entry(self)
-        self.vstup.grid(row=1,column=1)
+        self.vstup = tk.Entry(self)
+        self.vstup.grid(row=2,column=1,padx=10,pady=10)
+
+        self.listBox = tk.Listbox(self)
+        self.listBox.grid(row=3,column=1,padx=10,pady=10)
+
+        self.btn2 = tk.Button(self, text="Vložit", command=self.fce)
+        self.btn2.grid()
+        #self.bind("<Return>",self.zpracuj())
+    
 
         self.dva_operandy = {}
         self.dva_operandy["+"] = lambda a, b: a + b
@@ -39,76 +60,35 @@ class Application(tk.Tk):
 
         self.zasobnik= []
 
-    
 
+    def fce(self):
+        self.zpracuj(self.vstup.get())
 
-    def operace(self,token):
-        if token.upper() == "Q":
-            exit()
-        if token.upper() == "PI":
-            self.zasobnik.append(math.pi)
-        if token == "+":
-            b = self.zasobnik.pop()
-            a = self.zasobnik.pop()
-            self.zasobnik.append(a + b)
-        if token == "-":
-            b = self.zasobnik.pop()
-            a = self.zasobnik.pop()
-            self.zasobnik.append(a - b)
-        if token == "*":
-            b = self.zasobnik.pop()
-            a = self.zasobnik.pop()
-            self.zasobnik.append(a * b)
-        if token == "**":
-            b = self.zasobnik.pop()
-            a = self.zasobnik.pop()
-            self.zasobnik.append(a ** b)
-        if token == "sin":
-            a = self.zasobnik.pop()
-            self.zasobnik.append(math.sin(a))
-        if token == "cos":
-            a = self.zasobnik.pop()
-            self.zasobnik.append(math.cos(a))
-
-
-
-
-
-    def operace(self, token):
-        if token.upper() == "Q":
-            exit()
-        if token.upper() == "PI":
-            self.zasobnik.append(math.pi)
-        if token.upper() == "SW":
-            b = self.zasobnik.pop()
-            a = self.zasobnik.pop()
-            self.zasobnik.append(b)
-            self.zasobnik.append(a)
-        if token in self.dva_operandy.keys():
+    def operace(self, token, event=None):
+        token = str(self.vstup.get())
+        if token in dva_operandy.keys():
             if len(self.zasobnik) >= 2:
                 b = self.zasobnik.pop()
                 a = self.zasobnik.pop()
-                self.zasobnik.append(self.dva_operandy[token](a, b))
+                self.zasobnik.append(dva_operandy[token](a, b))
             else:
                 messagebox.showerror("Chyba", "Počet operandů je nedostačující.")
-        if token in self.jeden_operand.keys():
+        if token in jeden_operand.keys():
             if len(self.zasobnik) >= 1:
                 a = self.zasobnik.pop()
-                self.zasobnik.append(self.jeden_operand[token](a))
+                self.zasobnik.append(jeden_operand[token](a))
             else:
                 messagebox.showerror("Chyba", "Počet operandů je nedostačující.")
-
-
-    def zpracuj(self,radek):
-        tokeny = radek.split()
-        for token in tokeny:
-            try:
-                self.zasobnik.append(float(token))
-            except ValueError:
-                self.operace(token)
-
-    def ctu(self, token):
-        a = self.vstup.get()
+        self.listBox.delete(0,tk.END)
+        for token in self.zasobnik:
+            self.listBox.insert(END,token)
+        
+    def zpracuj(self,token):
+        try:
+            self.zasobnik.append(float(token))
+            self.listBox.insert(tk.END,token)
+        except ValueError:
+            self.operace(self,token)
 
 app = Application()
 app.mainloop()
